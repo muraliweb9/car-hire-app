0683db22175a3f5ef3e0b8ff58c3fd19b4461a64
muraliweb9 <muraliweb@hotmail.com>
2023-04-18 07:58:43 +0100

    Initial

--- src/main/java/com/interview/carhire/services/MaintenanceServiceProxy.java
+++ src/main/java/com/interview/carhire/services/MaintenanceServiceProxy.java
@@ -16,15 +16,27 @@ import java.util.List;
 @Slf4j
 public class MaintenanceServiceProxy {
 
+    /**
+     * Defined in this services application.yml
+     * api:
+     *   car-maintenance-app:
+     *     # This is the apring.application.name of the car-maintenance-app as registered with Eureka
+     *     baseurl: http://car-maintenance-app
+     */
     @Value("${api.car-maintenance-app.baseurl}")
     private String apiBaseUrl;
 
+    // Defined in com.interview.carhire.services.AppConfig
     @Autowired
     RestTemplate restTemplate;
 
     public MaintenanceRecord getMaintenanceRecord(String carId)
     {
         log.info("Looking up in car-maintenance-app for maintenance record for carId {}", carId);
+        // Constructs
+        // http://car-maintenance-app/maintenance/maintenancerecords/2
+        // Through Eureka http://car-maintenance-app maps to the car-maintenance-app
+        // This endpoint (/maintenance/maintenancerecords/2) is then hit
         MaintenanceRecord maintenanceRecord
                 = restTemplate.getForObject(apiBaseUrl + "/maintenance/maintenancerecords/" + carId, MaintenanceRecord.class);
 
